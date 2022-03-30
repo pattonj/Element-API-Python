c5d3526fda670fb7999e5ea3bedba165ff585724
Jacob Patton <jacob@pattons.org>
2022-03-30 09:12:13 -0400

    Added type hinting and updated examples

--- Element451API.py
+++ Element451API.py
@@ -2,23 +2,24 @@ import json
 import requests
 
 
-def api_user_search(client, api, feature, segment, project=["_id"]):
+def api_user_search(
+    client: str,
+    api: str,
+    feature: str,
+    segment: str | dict,
+    project: list[str] = ["_id"],
+) -> dict:
     """
     Provides a function to easily export users from an Element451 instance.
     **Uses the json and requests library.**
 
     Parameters:
         client (str): The name of the client instance
-
         api (str): The element API. Should be 'api.451.io'
-
         feature (str): The feature ID to access data
-
-        segment (str or list): The segment ID to export or list of users. Example: 'client.segments.40291' or ['620e9b3534e7f364a5726292']
-
+        segment (str or dict): The segment ID to export or inline segment. Example: 'client.segments.40291'
         ---- optional ----
         Project (list): The fields you want to list for each user. You can only project: [_id, first_name, last_name, email]. Defaults to [_id].
-
     Returns:
         The json data from the request. If more than 5,000 contacts were requested, the data is combined."""
 
@@ -67,7 +68,9 @@ def api_user_search(client, api, feature, segment, project=["_id"]):
     return all_user_payload_results
 
 
-def _create_api_search_payload(segment_id, project, last_id=""):
+def _create_api_search_payload(
+    segment_id: str, project: list[str], last_id: str = ""
+) -> dict:
     """Used to set the data for the search API request"""
     my_payload = json.dumps(
         {"item": {"segment": segment_id, "project": project, "last_id": last_id}}
@@ -75,27 +78,28 @@ def _create_api_search_payload(segment_id, project, last_id=""):
     return my_payload
 
 
-def api_data_request(client, api, feature, segment, template, column_key="", unwind={}):
+def api_data_request(
+    client: str,
+    api: str,
+    feature: str,
+    segment: str | list[str],
+    template: str | dict,
+    column_key: str = "",
+    unwind: dict = {},
+) -> dict:
     """
     Provides a function to easily export data from an Element451 instance.
     **Uses the json and requests library.**
 
     Parameters:
         client (str): The name of the client instance
-
         api (str): The element API. Should be 'api.451.io'
-
         feature (str): The feature ID to access data
-
         segment (str or list): The segment ID to export or list of users. Example: 'client.segments.40291' or ['620e9b3534e7f364a5726292']
-
         template (str or dict/list): The template ID or layout to export. Example: 'client.template.8071' or '{ "columns": [ { "field": "Email", "mode": "slug", "slug": "user-email-address" } ] }'
         ---- optional ----
         column_key (string): Can be set to either 'slug' or 'field. If empty, field is used.
-
         unwind (dict): The entity to unwind. Example: {"root" : "user-applications-root"}
-
-
     Returns:
         The json data from the request. If more than 50 files were requested, the data is combined.
     """
@@ -164,8 +168,12 @@ def api_data_request(client, api, feature, segment, template, column_key="", unw
 
 
 def _create_api_data_payload(
-    template_ID, segment, last_id="", column_key="", unwind={}
-):
+    template_ID: str | dict,
+    segment: str | list,
+    last_id: str = "",
+    column_key: str = "",
+    unwind: dict = {},
+) -> dict:
     """Used to create the data json request"""
     # Need to know if they passed a segment or a list.
     if isinstance(segment, list):
@@ -188,24 +196,21 @@ def _create_api_data_payload(
     return my_payload
 
 
-def api_data_import(client, api, feature, template, items):
+def api_data_import(
+    client: str, api: str, feature: str, template: str | dict, items: dict
+) -> dict:
     """
     Provides a function to easily import data to an Element451 instance.
     **Uses the json and requests library.**
 
     Parameters:
         client (str): The name of the client instance
-
         api (str): The element API. Should be 'api.451.io'
-
         feature (str): The feature ID to access data
-
         template (str or dict/list): Can be an existing template guid or a template can be passed inline. Example: 'client.template.40291' or
-
         Items: The data to be update.
             Example with passed template: '[{"0": "john.smith@example.com","1": "John","2": "Smith"},....]
             Example with template guid[ {"user-email-address": "john.smith@example.com","user-first-name": "John","user-last-name": "Smith"},...]
-
     Returns:
         The json data from the request. If more than 50 files were requested, the data is combined.
     """
@@ -231,70 +236,89 @@ if __name__ == "__main__":
     from dotenv import load_dotenv
 
     load_dotenv()
-    my_client = os.getenv("my_client")
-    my_api = os.getenv("my_api")
-    my_feature = os.getenv("my_feature")
-    my_segment = os.getenv("my_segment")
+    # The variable below are used for all import/export functions.
+    my_client = os.getenv("my_client")  # "client"
+    my_api = os.getenv("my_api")  # "api.451.io"
+    my_feature = os.getenv("my_feature")  # "1234abc456efg"
 
-    """Testing Export"""
-    # my_template = os.getenv("my_template_guid")
-    # # my_template={ "columns":
-    # #         [ { "field": "Email", "mode": "slug", "slug": "user-email-address" },
-    # #         { "field": "First Name", "mode": "slug", "slug": "user-first-name" },
-    # #          { "field": "Last Name", "mode": "slug", "slug": "user-last-name" }]#,
-    # #          #{ "field": "Note Type", "mode": "slug", "slug": "user-notes-note-type"} ]
-    # #          }
-    # column_key = "slug"
-    # # To use the unwind feature, uncomment the Note Type field in the template and the two fields below.
-    # # unwind = {"root" : "user-notes-root"}
-    # print(api_data_request(my_client, my_api, my_feature, my_segment, my_template))
+    """Export Example 1"""
+    my_segment = os.getenv("my_segment")  # "client.segments.4291"
+    my_template = os.getenv("my_template_guid")  # "client.template.8571"
+    print(api_data_request(my_client, my_api, my_feature, my_segment, my_template))
 
-    """Testing search"""
-    # project =["first_name"]
-    # print(api_user_search(my_client,my_api,my_feature,my_segment,project))
+    """Export Example 2"""
+    my_segment = os.getenv("my_segment")  # "client.segments.4291"
+    my_template = {
+        "columns": [
+            {"field": "Email", "mode": "slug", "slug": "user-email-address"},
+            {"field": "First Name", "mode": "slug", "slug": "user-first-name"},
+            {"field": "Last Name", "mode": "slug", "slug": "user-last-name"},
+            # { "field": "Note Type", "mode": "slug", "slug": "user-notes-note-type",
+        ]
+    }
+    column_key = "slug"
+    # To use the unwind feature, uncomment the Note Type field in the template and the field below, and add unwind to the function call.
+    # unwind = {"root" : "user-notes-root"}
+    print(
+        api_data_request(
+            my_client,
+            my_api,
+            my_feature,
+            my_segment,
+            my_template,
+        )
+    )
 
-    """Testing Import"""
-    # my_template = {
-    #     "columns": [
-    #         {
-    #             "field": "Email",
-    #             "mode": "slug",
-    #             "slug": "user-email-address",
-    #             "formula": "",
-    #             "transformations": [],
-    #             "scope": [],
-    #             "validations": [],
-    #             "type": "string",
-    #             "range": ["unique"],
-    #             "empty": "remove",
-    #         },
-    #         {
-    #             "field": "First Name",
-    #             "mode": "slug",
-    #             "slug": "user-first-name",
-    #             "formula": "",
-    #             "transformations": [],
-    #             "scope": [],
-    #             "validations": [],
-    #             "type": "string",
-    #             "range": [],
-    #             "empty": "remove",
-    #         },
-    #         {
-    #             "field": "Last Name",
-    #             "mode": "slug",
-    #             "slug": "user-last-name",
-    #             "formula": "",
-    #             "transformations": [],
-    #             "scope": [],
-    #             "validations": [],
-    #             "type": "string",
-    #             "range": [],
-    #             "empty": "remove",
-    #         },
-    #     ]
-    # }
-    # my_items = [
-    #     {"0": "johnsmith@email.com", "1": "John", "2": "Smith"}
-    # ]
-    # print(api_data_import(my_client, my_api, my_feature, my_template, my_items))
+    """Search Example"""
+    my_segment = os.getenv("my_segment")  # "client.segments.4291"
+    project = ["first_name"]
+    print(api_user_search(my_client, my_api, my_feature, my_segment, project))
+
+    """Import Example 1"""
+    my_template = os.getenv("my_template_guid")  # "client.templates.451",
+    my_items = [{"0": "johnsmith@email.com", "1": "John", "2": "Smith"}]
+    print(api_data_import(my_client, my_api, my_feature, my_template, my_items))
+
+    """Import Example 2"""
+    my_template = {
+        "columns": [
+            {
+                "field": "Email",
+                "mode": "slug",
+                "slug": "user-email-address",
+                "formula": "",
+                "transformations": [],
+                "scope": [],
+                "validations": [],
+                "type": "string",
+                "range": ["unique"],
+                "empty": "remove",
+            },
+            {
+                "field": "First Name",
+                "mode": "slug",
+                "slug": "user-first-name",
+                "formula": "",
+                "transformations": [],
+                "scope": [],
+                "validations": [],
+                "type": "string",
+                "range": [],
+                "empty": "remove",
+            },
+            {
+                "field": "Last Name",
+                "mode": "slug",
+                "slug": "user-last-name",
+                "formula": "",
+                "transformations": [],
+                "scope": [],
+                "validations": [],
+                "type": "string",
+                "range": [],
+                "empty": "remove",
+            },
+        ]
+    }
+    my_items = [{"0": "johnsmith@email.com", "1": "John", "2": "Smith"}]
+    print(api_data_import(my_client, my_api, my_feature, my_template, my_items))
